8073f1db8d91ed08ca8a5088d9c95d5600206845
Chitransh Jain <cjsmcs2810@gmail.com>
2021-01-10 14:28:05 +0530

    Final changes before deployment

--- app.js
+++ app.js
@@ -160,6 +160,11 @@ app.get("/:title", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-    console.log("Server is up and running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server is up and running");
 });
